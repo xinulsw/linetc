9a3fcf6aca950326174c26f766f7d37f39cdadea
xinulsw <xinulsw@gmail.com>
2017-03-04 11:22:35 +0100

    Kody szyfru Cezara

---
+++ cpp/szyfr_cezara.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using namespace std;
+
+void lower(char tekst[]) {
+/*
+* Funkcja zamienia duże litery na małe
+*/
+  int i = 0;
+  while (tekst[i]!='\0') {
+    if (tekst[i] == ' ') ;
+    else if ((int)tekst[i] > 64 && (int)tekst[i] < 90)
+      tekst[i] = (char)((int)tekst[i] + 32);
+    i++;
+  }
+}
+
+
+// A-Z = ASCII 65-90; a-z = ASCII 97-122; a-A = 32;
+void szyfruj(char tekst[], int klucz) {
+  int i = 0;
+  klucz = klucz % 26;	// przesuniecie znaku o calkowita wielokrotnosc 26 nie zmienia kodowanego znaku!
+  while (tekst[i]!='\0') {
+    if (tekst[i] == ' ') ;
+    else if ((int)tekst[i] + klucz > 122) // ograniczenie, aby nie przekroczyc zakresu 122 w ASCII
+      tekst[i] = (char)((int)tekst[i] + klucz - 26);
+    else
+      tekst[i] = (char)((int)tekst[i] + klucz);
+    i++;
+  }
+}
+
+void deszyfruj(char tekst[], int klucz) {
+  int i = 0;
+  klucz = klucz%26;
+  while (tekst[i]!='\0') {
+    if (tekst[i] == ' ') ;
+    else if ((int)tekst[i] - klucz < 97)
+      tekst[i] = (char)((int)tekst[i] - klucz + 26);
+    else
+      tekst[i] = (char)((int)tekst[i] - klucz);
+    i++;
+  }
+}
+
+int main(int argc, char *argv[]) {
+  #define MAKS 100
+  char tekst[MAKS];
+  int klucz;
+  cout << "Podaj tekst: " << endl;
+  cin.getline(tekst, MAKS);
+  lower(tekst);
+  cout << "Podaj klucz: ";
+  cin >> klucz;
+  szyfruj(tekst, klucz);
+  cout << "Szyfrogram:" << endl << tekst << endl;
+  deszyfruj(tekst, klucz);
+  cout << "DEszyfr:" << endl << tekst << endl;
+  // getchar();
+  return 0;
+}
--- docs/python/algorytmy/szyfr_cezara.py
+++ docs/python/algorytmy/szyfr_cezara.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 #
 # Implementacja klasycznego szyfru Cezara
 # <eCG>
@@ -7,57 +7,69 @@
 """
     ZADANIE
     Pobierz od użytkownika ciąg znaków, zaszyfruj go przy wykorzystaniu
-    szyfru Cezara o kluczu 3, wyświetl zaszyfrowany ciąg.
+    szyfru Cezara o kluczu podanym przez użytkownika,
+    wyświetl zaszyfrowany ciąg. Następnie zdeszyfruj go
+    i ponownie wyświetl wynik operacji.
 """
 
-KLUCZ = 3
 
-
-def szyfruj(txt):
-    stxt = ""
-    for i in range(len(txt)):
-        if ord(txt[i]) > 122 - KLUCZ:
-            stxt += chr(ord(txt[i]) + KLUCZ - 26)
+def szyfruj(tekst, klucz):
+    szyfrogram = ""
+    for znak in tekst:
+        znak = znak.lower()
+        if znak == ' ':
+            pass
+        elif ord(znak) + klucz > 122:
+            znak = chr(ord(znak) + klucz - 26)
         else:
-            stxt += chr(ord(txt[i]) + KLUCZ)
-    return stxt
+            znak = chr(ord(znak) + klucz)
+        szyfrogram += znak
+    return szyfrogram
 
 
-utxt = raw_input("Podaj ciąg do zaszyfrowania:\n")
-stxt = szyfruj(utxt)
-print "Ciąg zaszyfrowany:\n", stxt
+def deszyfruj(szyfrogram, klucz):
+    tekst = ""
+    for znak in szyfrogram:
+        if znak == ' ':
+            pass
+        elif ord(znak) - klucz < 97:
+            znak = chr(ord(znak) - klucz + 26)
+        else:
+            znak = chr(ord(znak) - klucz)
+        tekst += znak
+    return tekst
+
+
+def main(args):
+    tekst = raw_input("Podaj tekst do zaszyfrowania:\n")
+    try:
+        klucz = int(raw_input("Podaj klucz:\n"))
+        klucz = klucz % 26
+        szyfrogram = szyfruj(tekst, klucz)
+        deszyfr = deszyfruj(szyfrogram, klucz)
+        assert deszyfr == tekst.lower()
+        print "Szyfrogram:\n", szyfrogram
+        print "Deszyfracja:\n", deszyfr
+    except ValueError:
+        print "Błędny klucz!"
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
 
 """
     JAK TO DZIAŁA
-    W programie możemy wykorzystywać zmienne globalne, np. KLUCZ.
-    def nazwa_funkcji(argumenty): - tak definiujemy funkcje, które
-    mogą lub nie zwracać jakieś wartości.
-    nazwa_funkcji(argumenty) - tak wywołujemy funkcje.
+    try ... except to konstrukcja która przechwytuje błędy: w bloku try
+    użytkownik może wprowadzić niewłaściwy klucz, np. literę, wtedy wykona się
+    kod w bloku except.
+    def nazwa_funkcji(argumenty): – tak definiujemy funkcje
+    return zmienna – instrukcja zwraca wartość przypisaną zmiennej
+    nazwa_funkcji(argumenty) – tak wywołujemy funkcje.
     Napisy mogą być indeksowane (od 0), co daje dostęp do pojedynczych znaków.
     Funkcja len(str) zwraca długość napisu, wykorzystana jako argument funkcji
     range() pozwala iterować po znakach napisu.
     Operator += oznacza dodanie argumentu z prawej strony do wartości z lewej.
-"""
-
-"""
-    CO MOGĘ ZMIENIĆ
-    Napisz funkcję deszyfrującą deszyfruj(txt).
-    Dodaj do funkcji szyfruj, deszyfruj drugi parametr w postaci długości
-    klucza podawanej przez użytkownika.
-    Dodaj poprawne szyfrowanie dużych liter, białych znaków i znaków
-    interpunkcyjnych.
-
-def deszyfruj(txt):
-    dtxt = ""
-    KLUCZM = KLUCZ % 26;
-    for i in range(len(txt)):
-        if (ord(txt[i]) - KLUCZM < 97):
-            dtxt += chr(ord(txt[i]) - KLUCZM + 26)
-        else:
-            dtxt += chr(ord(txt[i]) - KLUCZM)
-    return dtxt
-
-dtxt = deszyfruj(stxt)
-print "Ciąg zdeszyfrowany:\n", dtxt
-
 """
