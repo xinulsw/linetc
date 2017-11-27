f06cc26a3fcbd9cd6245e54598444215371cfba6
lo1cgsan <lo1cgsan@gmail.com>
2017-11-27 23:01:05 +0100

    fibonacci w python3 + uzupełnienia

--- docs/python/algorytmy/fibonacci.py
+++ docs/python/algorytmy/fibonacci.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
 # Ciąg Fibonacciego
 # <eCG>
+
+
+def fib_iter0(n):
+    """Funkcja zwraca n-ty wyraz ciągu Fibonacciego
+       F(0) = 0
+       F(1) = 1
+       F(n) = F(n-2) + F(n-1) dla n > 1
+    """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = (0, 1)
+    for i in range(1, n - 1):
+        a, b = b, a + b
+    return b
 
 
 def fib_iter1(n):  # definicja funkcji
@@ -11,11 +27,11 @@ def fib_iter1(n):  # definicja funkcji
         aż do wyrazu n-tego, który zwraca.
         Wersja iteracyjna z pętlą while.
     """
-    pwyrazy = (0, 1)  # dwa pierwsze wyrazy ciągu zapisane w tupli
-    a, b = pwyrazy  # przypisanie wielokrotne, rozpakowanie tupli
-    print a,
+    # dwa pierwsze wyrazy ciągu zapisane w tupli
+    a, b = (0, 1)  # przypisanie wielokrotne, rozpakowanie tupli
+    print(a, end=' ')
     while n > 1:
-        print b,
+        print(b, end=' ')
         a, b = b, a + b  # przypisanie wielokrotne
         n -= 1
 
@@ -27,14 +43,14 @@ def fib_iter2(n):
         Wersja iteracyjna z pętlą for.
     """
     a, b = 0, 1
-    print "wyraz", 1, a
-    print "wyraz", 2, b
+    print("wyraz", 1, a)
+    print("wyraz", 2, b)
     for i in range(1, n - 1):
         # wynik = a + b
         a, b = b, a + b
-        print "wyraz", i + 2, b
+        print("wyraz", i + 2, b)
 
-    print ""  # wiersz odstępu
+    print()  # wiersz odstępu
     return b
 
 
@@ -51,13 +67,15 @@ def fib_rek(n):
 
 
 def main(args):
-    n = int(raw_input("Podaj nr wyrazu: "))
+    n = int(input("Podaj nr wyrazu: "))
+    print("Wyraz {:d}: {:d}".format(n, fib_iter0(n)))
+    print("=" * 40)
     fib_iter1(n)
-    print ""
-    print "=" * 40
+    print()
+    print("=" * 40)
     fib_iter2(n)
-    print "=" * 40
-    print fib_rek(n - 1)
+    print("=" * 40)
+    print(fib_rek(n - 1))
     return 0
 
 
