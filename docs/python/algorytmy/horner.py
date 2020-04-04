ef828468cef5139e3f3407977ae2fa8b30c58e3a
xinulsw <xinulsw@gmail.com>
2020-04-04 10:49:19 +0200

    aktualizacja hornera

--- docs/python/algorytmy/horner.py
+++ docs/python/algorytmy/horner.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Program oblicza wartość wielomianu 3 stopnia przy użyciu schematu Hornera.
+# Program oblicza wartość wielomianu n-stopnia przy użyciu schematu Hornera.
 # <eCG>
 
 
-def horner(tabwsp, x, stopien):
+def horner_it(tabwsp, x, stopien):
     wartosc = tabwsp[0]
-    for i in range(1, 4, 1):
+    for i in range(1, stopien + 1):
+        print(wartosc, tabwsp[i])
         wartosc = wartosc * x + tabwsp[i]
+        print(wartosc)
     return wartosc
 
 
-def main(args):
-    stopien = 3
-    tabwsp = []  # pusta lista
-    print "Podaj 4 współczynniki: "
-    for i in range(stopien + 1):
-        liczba = raw_input("Podaj współczynnik: ")
-        tabwsp.append(float(liczba))
-    x = float(raw_input("Podj argument: "))
+def horner_re(tabwsp, x, stopien):
+    if (stopien == 0):
+        return tabwsp[0]
+    else:
+        return horner_re(tabwsp, x, stopien - 1) * x + tabwsp[stopien]
 
-    print tabwsp, x
-    print "Wartość:", horner(tabwsp, x, stopien)
+
+def main(args):
+    stopien = int(input("Podaj stopień: "))
+    tabwsp = []  # pusta lista
+    print("Podaj współczynniki: ")
+    for i in range(stopien + 1):
+        liczba = input(
+            "Podaj współczynnik przy potędze {}: ".format(stopien - i))
+        tabwsp.append(float(liczba))
+    x = float(input("Podj argument: "))
+
+    for i in range(stopien + 1):
+        print(str(int(tabwsp[i])) + 'x^' + str(stopien-i) + '+', end='')
+    print (tabwsp, x)
+    print ("Wartość:", horner_it(tabwsp, x, stopien))
+    print ("Wartość:", horner_re(tabwsp, x, stopien))
 
     return 0
 
